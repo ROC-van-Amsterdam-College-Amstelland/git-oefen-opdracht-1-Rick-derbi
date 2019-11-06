8ee2e38638fa0ea53eb51df78f833b5e7fe6d22a
Rick-derbi <2107490@talnet.nl>
2019-11-06 12:16:36 +0100

    last commit 12:16

--- js/java.js
+++ js/java.js
@@ -1,3 +1,11 @@
-<script src="stylesheet.js"></script>
-var d = new Date();
-document.write(d)
+    
+    function setDate()
+    {
+        var d = new Date();
+        var date = d.getDate();
+        var year = d.getFullYear();
+        var month = d.getMonth();
+        var monthArr = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+        month = monthArr[month];
+        document.getElementById("date").innerHTML=date+" "+month+", "+year;
+    }
